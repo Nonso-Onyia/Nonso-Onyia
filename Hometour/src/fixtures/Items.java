d618a993031a4f0bd31825ac743178d7abf58abe
Chukwunonso Onyia <88759766+Nonso-Onyia@users.noreply.github.com>
2021-09-15 17:01:47 -0400

    Delete Hometour directory

--- Hometour/src/fixtures/Items.java
+++
@@ -1,9 +0,0 @@
-package fixtures;
-
-public class Items extends Fixture {
-
-	public Items(String name, String shortDescription, String longDescription) {
-		super(name, shortDescription, longDescription); 
-	}
-
-}
